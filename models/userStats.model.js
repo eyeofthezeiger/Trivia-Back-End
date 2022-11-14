2eccbf5a2b67dc81ea97aa49e62b8274d7174e53
stillwiggle <mike.stillwagon@gmail.com>
2022-11-14 16:03:22 -0500

    stats-virtuals: Added a virtual field to the user stats model.  Tests in Postman and Swagger successful.

--- models/userStats.model.js
+++ models/userStats.model.js
@@ -1,6 +1,9 @@
 //bring in mongoose so we can create a schema that represents the data for a User Stats
 const mongoose = require("mongoose");
 
+// Used to include the virtual field in the JSON response.  Virtuals not included by default.
+const opts = { toJSON: { virtuals: true } };
+
 //Create our schema using mongoose that contains the fields and their data types for our User Stats
 //More info: https://mongoosejs.com/docs/schematypes.html
 const userStatsSchema = new mongoose.Schema({
@@ -35,8 +38,12 @@ const userStatsSchema = new mongoose.Schema({
         },
         match: [/.+\@.+\..+/, "Invalid E-mail Address"],
     }
-})
+}, opts)
 
+// Virtual field to calculate the win/loss ratio
+userStatsSchema.virtual('winRatio').get(function() {
+    return this.correctAnswers / this.questionsAttempted;
+  });
 
 //Generate the model our code with interact with from the above schema
 //Models allow us to interact with the data inside our MongoDB collections
